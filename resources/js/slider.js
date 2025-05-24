886ec723501f565aa1bfa17e198af47b9dd0f926
Jzherox <eliasballadares2004@gmail.com>
2025-05-23 19:56:02 -0500

    general responsive test 3

--- resources/js/slider.js
+++ resources/js/slider.js
@@ -1,20 +1,21 @@
-const track = document.getElementById('sliderTrack');
-const slideWidth = 300;
-let index = 0;
-const maxIndex = 4;
+    const track = document.getElementById('sliderTrack');
+    const slideWidth = 300;
+    let index = 0;
+    const maxIndex = 4; // cantidad de slides originales
 
-function moveSlider() {
-  index++;
-  track.style.transition = 'transform 1s ease-in-out';
-  track.style.transform = `translateX(-${slideWidth * index}px)`;
+    function moveSlider() {
+      index++;
+      track.style.transition = 'transform 0.5s ease-in-out';
+      track.style.transform = `translateX(-${slideWidth * index}px)`;
 
-  if (index === maxIndex) {
-    setTimeout(() => {
-      track.style.transition = 'none';
-      index = 0;
-      track.style.transform = 'translateX(0)';
-    }, 500);
-  }
-}
+      if (index === maxIndex) {
+        // Al llegar al último slide (index=4), reiniciar al inicio sin transición
+        setTimeout(() => {
+          track.style.transition = 'none';
+          index = 0;
+          track.style.transform = 'translateX(0)';
+        }, 500); // debe coincidir con la duración de la transición
+      }
+    }
 
-setInterval(moveSlider, 7000);
+    setInterval(moveSlider, 5000);
